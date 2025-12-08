1e29a57899b45d5273624f54c2283e74565da179
Jean Silva <jeanbarrossilva@outlook.com>
2025-12-08 07:40:49 -0300

    Move quark declarations to single file

--- QuantumMechanics/Elementary/Quark.swift
+++ QuantumMechanics/Elementary/Quark.swift
@@ -15,6 +15,203 @@
 // not, see https://www.gnu.org/licenses.
 // ===-------------------------------------------------------------------------------------------===
 
+/// Heaviest ``Quark``, with a Lagrangian mass of 173.21 ± 0.51 ± 0.7 GeV/*c*². Decays to a
+/// ``BottomQuark``.
+struct TopQuark<ColorLike: SingleColor>: Quark {
+  public let symbol = "t"
+  public let charge = ElectricCharge.elementary(2 / 3)
+  public let colorLike: ColorLike
+
+  public init(colorLike: ColorLike) { self.colorLike = colorLike }
+
+  func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    approximator.approximate(
+      .gigaelectronvoltsPerLightSpeedSquared(173.21),
+      .gigaelectronvoltsPerLightSpeedSquared(0.51),
+      .gigaelectronvoltsPerLightSpeedSquared(0.7)
+    )
+  }
+}
+
+/// Second heaviest ``Quark``, with a Lagrangian mass of 4.18 ± 0.03 GeV/*c*². Decays to a
+/// ``CharmQuark``.
+public struct BottomQuark<ColorLike: SingleColor>: Quark {
+  public let symbol = "b"
+  public let charge = ElectricCharge.elementary(-1 / 3)
+  public let colorLike: ColorLike
+
+  public init(colorLike: ColorLike) { self.colorLike = colorLike }
+
+  public func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    approximator.approximate(
+      .gigaelectronvoltsPerLightSpeedSquared(4.18),
+      .gigaelectronvoltsPerLightSpeedSquared(0.03),
+      .zero
+    )
+  }
+}
+
+/// Third heaviest ``Quark``, with a Lagrangian mass of 1.275 ± 0.025 GeV/*c*². Decays to a
+/// ``StrangeQuark``.
+public struct CharmQuark<ColorLike: SingleColor>: Quark {
+  public let symbol = "c"
+  public let charge = ElectricCharge.elementary(2 / 3)
+  public let colorLike: ColorLike
+
+  public init(colorLike: ColorLike) { self.colorLike = colorLike }
+
+  public func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    approximator.approximate(
+      .gigaelectronvoltsPerLightSpeedSquared(1.275),
+      .megaelectronvoltsPerLightSpeedSquared(25),
+      .zero
+    )
+  }
+}
+
+/// Third lightest ``Quark``, with a Lagrangian mass of 95 ± 5 MeV/*c*². Decays to a ``DownQuark``.
+public struct StrangeQuark<ColorLike: SingleColor>: Quark {
+  public let symbol = "s"
+  public let charge = ElectricCharge.elementary(-1 / 3)
+  public let colorLike: ColorLike
+
+  public init(colorLike: ColorLike) { self.colorLike = colorLike }
+
+  public func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    approximator.approximate(
+      .megaelectronvoltsPerLightSpeedSquared(95),
+      .gigaelectronvoltsPerLightSpeedSquared(5),
+      .zero
+    )
+  }
+}
+
+/// Second lightest ``Quark``, with a Lagrangian mass of 4.8 ± 0.5 ± 0.3 MeV/*c*². Decays to an
+/// ``UpQuark``.
+public struct DownQuark<ColorLike: SingleColor>: Quark {
+  public let symbol = "d"
+  public let charge = ElectricCharge.elementary(-1 / 3)
+  public let colorLike: ColorLike
+
+  public init(colorLike: ColorLike) { self.colorLike = colorLike }
+
+  public func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    approximator.approximate(
+      .megaelectronvoltsPerLightSpeedSquared(4.8),
+      .megaelectronvoltsPerLightSpeedSquared(0.5),
+      .megaelectronvoltsPerLightSpeedSquared(0.3)
+    )
+  }
+}
+
+/// Lightest ``Quark``, with a Lagrangian mass of 2.3 ± 0.7 ± 0.5 MeV/*c*². As per the Standard
+/// Model, cannot decay.
+public struct UpQuark<ColorLike: SingleColor>: Quark {
+  public let symbol = "u"
+  public let charge = ElectricCharge.elementary(2 / 3)
+  public let colorLike: ColorLike
+
+  public init(colorLike: ColorLike) { self.colorLike = colorLike }
+
+  public func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    approximator.approximate(
+      .megaelectronvoltsPerLightSpeedSquared(2.3),
+      .megaelectronvoltsPerLightSpeedSquared(0.7),
+      .megaelectronvoltsPerLightSpeedSquared(0.5)
+    )
+  }
+}
+
+/// ``QuarkLike`` whose flavor information has been erased.
+public struct AnyQuarkLike: Discrete, QuarkLike {
+  /// ``Quark`` based on which this one was initialized.
+  let base: any QuarkLike & Sendable
+
+  public static let discretion = AnyQuark.discretion.flatMap { quark in
+    [Self.init(quark), .init(Anti(quark))]
+  }
+
+  public let spin: Spin
+  public let charge: ElectricCharge
+  public let symbol: String
+  public let colorLike: AnySingleColorLike
+
+  public init(_ base: some QuarkLike) {
+    if let base = base as? Self {
+      self = base
+    } else if let base = base as? AnyQuark {
+      self.base = base.base
+      spin = base.spin
+      charge = base.charge
+      symbol = base.symbol
+      colorLike = .init(base.colorLike)
+    } else {
+      self.base = base
+      spin = base.spin
+      charge = base.charge
+      symbol = base.symbol
+      colorLike = .init(base.colorLike)
+    }
+  }
+
+  public func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    base.getMass(approximatedBy: approximator)
+  }
+}
+
+extension AnyQuarkLike: Equatable {
+  public static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs._coloredParticleLikeIsPartiallyEqual(to: rhs)
+  }
+}
+
+/// ``Quark`` whose flavor information has been erased.
+public struct AnyQuark: Discrete, Quark {
+  /// ``Quark`` based on which this one was initialized.
+  let base: any QuarkLike
+
+  public static let discretion = AnySingleColor.discretion.flatMap { color in
+    [
+      Self.init(UpQuark(colorLike: color)), .init(DownQuark(colorLike: color)),
+      .init(CharmQuark(colorLike: color)), .init(StrangeQuark(colorLike: color)),
+      .init(BottomQuark(colorLike: color)), .init(TopQuark(colorLike: color))
+    ]
+  }.sorted(by: <)
+
+  public let spin: Spin
+  public let charge: ElectricCharge
+  public let symbol: String
+  public let colorLike: AnySingleColor
+
+  public init(_ base: some Quark) {
+    if let base = base as? Self {
+      self = base
+    } else if let base = base as? AnyQuarkLike, let color = base.colorLike as? any SingleColor {
+      self.base = base.base
+      spin = base.spin
+      charge = base.charge
+      symbol = base.symbol
+      colorLike = .init(color)
+    } else {
+      self.base = base
+      spin = base.spin
+      charge = base.charge
+      symbol = base.symbol
+      colorLike = .init(base.colorLike)
+    }
+  }
+
+  public func getMass(approximatedBy approximator: Approximator<Mass>) -> Mass {
+    base.getMass(approximatedBy: approximator)
+  }
+}
+
+extension AnyQuark: Equatable {
+  public static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs._coloredParticleLikeIsPartiallyEqual(to: rhs)
+  }
+}
+
 /// A quark (q) is an elementary fermion ``ColoredParticle`` which is confined, bound to at least
 /// another one by gluon ``Particle``s via strong force. It is the only ``Particle`` in the Standard
 /// Model which experiences each of the four fundamental forces: strong, weak, electromagnetic and
