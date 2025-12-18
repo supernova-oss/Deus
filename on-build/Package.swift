2853f1e70be89c1f1f146a1d9f78eff84c3e656b
Jean Silva <jeanbarrossilva@outlook.com>
2025-12-18 17:05:16 -0300

    Assert toolchain installation and usage

--- on-build/Package.swift
+++ on-build/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 // ===-------------------------------------------------------------------------------------------===
 // Copyright © 2025 Supernova. All rights reserved.
 //
