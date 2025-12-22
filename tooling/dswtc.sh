877077b95266a29733800b21cba8d024a1c1701a
Jean Silva <jeanbarrossilva@outlook.com>
2025-12-22 16:05:05 -0300

    Add license notice header to `tooling/dswtc.sh`

--- tooling/dswtc.sh
+++ tooling/dswtc.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+# ===--------------------------------------------------------------------------------------------===
+# Copyright © 2025 Supernova. All rights reserved.
+#
+# This file is part of the Deus open-source project.
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not,
+# see https://www.gnu.org/licenses.
+# ===--------------------------------------------------------------------------------------------===
+
+# dswtc is the Deus Swift toolchain. Swift toolchains are a set of tools for documenting, linking,
+# compiling and debugging Swift sources. Deus requires a version of the Swift toolchain which
+# supports automatic differentiation for implementing quantum fields. The version of dswtc is that
+# defined by the .swift-version file located at the root of the directory of the project.
+#
+# This script provides information about the dswtc — or no information at all, in case
+# is_dswtc_installed equals zero.
 
 project_directory="$(dirname "$(realpath "$0")")"/..
 dswtc_version="$(cat "$project_directory"/.swift-version | xargs)"
