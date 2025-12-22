5835f856b11a2db99d7d1ea99dc181fa6e2f4adb
Jean Silva <jeanbarrossilva@outlook.com>
2025-12-22 12:00:47 -0300

    Remove non-POSIX-compliant `echo -e` from `assert.sh`

--- tooling/assert.sh
+++ tooling/assert.sh
@@ -25,18 +25,18 @@
 # - removal of array equality and inequality asserters; and
 # - replacement of header comment describing the program by the MIT license notice.
 
-if command -v tput &>/dev/null && tty -s; then
+if command -v tput >/dev/null 2>&1 && tty -s; then
   RED=$(tput setaf 1)
   GREEN=$(tput setaf 2)
   MAGENTA=$(tput setaf 5)
   NORMAL=$(tput sgr0)
   BOLD=$(tput bold)
 else
-  RED=$(echo -en "\e[31m")
-  GREEN=$(echo -en "\e[32m")
-  MAGENTA=$(echo -en "\e[35m")
-  NORMAL=$(echo -en "\e[00m")
-  BOLD=$(echo -en "\e[01m")
+  RED=""
+  GREEN=""
+  MAGENTA=""
+  NORMAL=""
+  BOLD=""
 fi
 
 log_header() {
